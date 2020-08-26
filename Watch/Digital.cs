72ed6651e28eeff92cb596d89c94798595af1ce0
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2020-08-26 15:58:14 +0200

    Test project

---
+++ Watch/Digital.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Revuo.Home.Abstractions;
+
+namespace Watch
+{
+    public class Digital : ILogic
+    {
+        private int _counter = 0;
+        private string WindowId = null;
+        private static Timer timer;
+
+        public async Task<IResult> Do(IContext context)
+        {
+            if (timer == null)
+            {
+                timer = new Timer(state =>
+                {
+                    _counter++;
+
+                    var result =
+                        context.Run("Revuo.Home.Components.Box",
+                            new Request(
+                                WindowId, 
+                                "Timer",
+                                DateTime.Now.ToString()
+                                ))
+                            .Result;
+
+                    if (result.Success &&
+                        WindowId == null)
+                    {
+                        WindowId = result.Data<string>(0);
+                    }
+                }, _counter, 0, 1000);
+            }
+
+            return new Result();
+        }
+    }
+}
