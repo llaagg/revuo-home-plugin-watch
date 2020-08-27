06335c4fe858edbc2bbdd8469bdeba0bfe52bc6c
Paweł Gałęzowski <pgalezowski@gmail.com>
2020-08-27 15:52:05 +0200

    Create Console.cs

---
+++ Watch/Console.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Revuo.Home.Abstractions;
+
+namespace Watch
+{
+    public class Console : ILogic
+    {
+        private int _counter = 0;
+        private string WindowId = null;
+        private static Timer timer;
+
+        public async Task<IResult> Do(IContext context)
+        {
+            Console.Log( DateTime.Now.ToString());
+            return new Result();
+        }
+    }
+}
