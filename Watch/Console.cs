d248e4cd1d3411b4eb9e798047d583dde5dbf862
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2020-10-10 19:38:50 +0200

    add console

--- Watch/Console.cs
+++ Watch/Console.cs
@@ -1,20 +1,24 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Revuo.Home.Abstractions;
 
 namespace Watch
 {
-    public class Console : ILogic
+    public class Console : ILogic, IInfo
     {
-        private int _counter = 0;
-        private string WindowId = null;
-        private static Timer timer;
-
         public async Task<IResult> Do(IContext context)
         {
             System.Console.WriteLine( DateTime.Now.ToString());
             return new Result();
+        }
+
+        public List<KeyValuePair<string, string>> GetInfo()
+        {
+            return new List<KeyValuePair<string, string>>(){
+                new KeyValuePair<string, string>(MetaData.StartAble, "true")
+            };
         }
     }
 }
