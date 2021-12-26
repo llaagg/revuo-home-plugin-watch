c349e0f43fc24f1a215ad27050e70e9daa6de219
Paweł Gałęzowski <pgalezowski@gmail.com>
2021-12-26 20:47:21 +0100

    added textBCD

---
+++ Watch/TextBCD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Revuo.Home.Abstractions;
+
+namespace Watch
+{
+    public class TextBCD : ILogic, IInfo
+    {
+        private int _counter = 0;
+        private string WindowId = null;
+        private static Timer timer;
+
+        public async Task<IResult> Do(IContext context)
+        {
+            if (timer == null)
+            {
+                timer = new Timer(state =>
+                {
+                    _counter++;
+
+                    var result =
+                        context.Run("Revuo.Home.Components.UI.Control",
+                            new Request(
+                                WindowId, 
+                                new
+                                {
+                                    Title = "Analog watch",
+                                    TypeName =typeof(AnalogueWatch).AssemblyQualifiedName
+
+                                },
+                                $"BCD {DateTime.Now.ToString()}"
+                                ))
+                            .Result;
+
+                    if (result.Success)
+                    {
+                        WindowId = result.Data<string>(0);
+                    }
+                }, _counter, 0, 1000);
+            }
+
+            return new Result();
+        }
+
+        public List<KeyValuePair<string, string>> GetInfo()
+        {
+            return new List<KeyValuePair<string, string>>(){
+                new KeyValuePair<string, string>(MetaData.StartAble, "true")
+            };
+        }
+
+    }
+}
